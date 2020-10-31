1f0acaf7649a2007fcb9596b30b9b61abbb3ac9e
Kevin Vu <kevinvu59@gmail.com>
2020-10-31 03:21:00 -0400

    Refactor text field delegate to be the cell itself. Add protocol declaration for delegate that will save textfield data to the VC

--- RemindMe/Scenes/AddReminderAlert/Views/NameOfReminderCell.swift
+++ RemindMe/Scenes/AddReminderAlert/Views/NameOfReminderCell.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol NameOfReminderCellDelegate: class {
+    func setReminderName(_ name: String)
+}
+
 class NameOfReminderCell: UITableViewCell {
     
     // MARK: Constants
@@ -15,11 +19,13 @@ class NameOfReminderCell: UITableViewCell {
     
     
     // MARK: Properties/UI
-    let textField: UITextField = {
+    weak var didFinishEditingReminderNameDelegate: NameOfReminderCellDelegate?
+    
+    lazy var textField: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
         tf.placeholder = "Name of reminder..."
-        
+        tf.delegate = self
         return tf
     }()
     
@@ -36,9 +42,28 @@ class NameOfReminderCell: UITableViewCell {
     // MARK: Setup
     private func setup() {
         contentView.addSubview(textField)
-        
         textField.setAndActivateConstraints(top: topAnchor, bottom: bottomAnchor, leading: leadingAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
     }
 }
 
-
+// MARK: Text field delegate
+extension NameOfReminderCell: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            textField.text = "Untitled"
+        }
+        textField.selectAll(nil)
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        guard let nameOfReminder = textField.text else { return true }
+        didFinishEditingReminderNameDelegate?.setReminderName(nameOfReminder)
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
